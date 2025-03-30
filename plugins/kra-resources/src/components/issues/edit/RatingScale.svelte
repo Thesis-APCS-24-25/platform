4d8be0dcb96d5c23f0ec7cb3c5c35b522c7346f7
ltp1405 <lamtungpro1405@gmail.com>
2025-03-30 13:40:13 +0700

    split components

--- plugins/kra-resources/src/components/issues/edit/RatingScale.svelte
+++ plugins/kra-resources/src/components/issues/edit/RatingScale.svelte
@@ -1,20 +1,9 @@
 <script lang="ts">
   import { RatingScale } from '@hcengineering/kra'
   import { EditBox } from '@hcengineering/ui'
+  import RatingScaleBoxes from './RatingScaleBoxes.svelte'
 
   export let ratingScale: RatingScale
-
-  // Array of colors from negative (red) to positive (green)
-  const ratingColors = [
-    'var(--theme-rating-very-poor)',
-    'var(--theme-rating-very-poor)',
-    'var(--theme-rating-poor)',
-    'var(--theme-rating-average)',
-    'var(--theme-rating-good)',
-    'var(--theme-rating-excellent)'
-  ]
-
-  const colorI = ratingScale.value ?? 0
 </script>
 
 <div class="container">
@@ -24,15 +13,8 @@
     <EditBox kind="small-style" disabled={true} value={ratingScale.description} />
   </div>
 
-  <div class="rating-container">
-    {#each Array(5) as _, i}
-      <div
-        class="rating-box"
-        class:filled={i < (ratingScale.value ?? 0)}
-        style={i < (ratingScale.value ?? 0) ? `background-color: ${ratingColors[colorI]}; border-color: ${ratingColors[colorI]};` : ''}
-      ></div>
-    {/each}
-  </div>
+  <RatingScaleBoxes value={ratingScale.value} />
+
 </div>
 
 <style>
@@ -44,24 +26,5 @@
 
   .header {
     flex-grow: 2;
-  }
-
-  .rating-container {
-    display: flex;
-    align-items: center;
-    gap: 0.5rem;
-  }
-
-  .rating-box {
-    width: 1rem;
-    height: 1.5rem;
-    border: 1px solid #ccc;
-    border-radius: 0.25rem;
-    background-color: #f5f5f5;
-    transition: all 0.2s ease-in-out;
-  }
-
-  .rating-box.filled {
-    border-color: transparent;
   }
 </style>
---
+++ plugins/kra-resources/src/components/issues/edit/RatingScaleBoxes.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+  const ratingColors = [
+    'var(--theme-rating-very-poor)', // dummy value
+    'var(--theme-rating-very-poor)',
+    'var(--theme-rating-poor)',
+    'var(--theme-rating-average)',
+    'var(--theme-rating-good)',
+    'var(--theme-rating-excellent)'
+  ]
+
+  export let value: number | null = null
+  value = value ?? 0
+
+</script>
+
+{#if value}
+  <div class="rating-container">
+    {#each Array(5) as _, i}
+      <div
+        class="rating-box"
+        class:filled={i < (value ?? 0)}
+        style={i < (value ?? 0)
+          ? `background-color: ${ratingColors[value]}; border-color: ${ratingColors[value]};`
+          : ''}
+      ></div>
+    {/each}
+  </div>
+{/if}
+
+<style lang="scss">
+  .rating-container {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+  }
+
+  .rating-box {
+    width: 1rem;
+    height: 1.5rem;
+    border: 1px solid #ccc;
+    border-radius: 0.25rem;
+    background-color: #f5f5f5;
+    transition: all 0.2s ease-in-out;
+  }
+
+  .rating-box.filled {
+    border-color: transparent;
+  }
+</style>
