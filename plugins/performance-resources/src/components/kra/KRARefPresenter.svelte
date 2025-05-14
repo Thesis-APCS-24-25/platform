bf6c6487164183b482d7daa47d84118e0d88c0ae
nhuhoainam <hoainamnguyennhu7@gmail.com>
2025-05-14 10:28:28 +0700

    fix: add conditional rendering for KRA reference display

--- plugins/performance-resources/src/components/kra/KRARefPresenter.svelte
+++ plugins/performance-resources/src/components/kra/KRARefPresenter.svelte
@@ -3,6 +3,7 @@
   import { KRA } from '@hcengineering/performance'
   import ObjectPresenter from '@hcengineering/view-resources/src/components/ObjectPresenter.svelte'
   import performance from '../../plugin'
+  import { Label } from '@hcengineering/ui'
 
   export let value: Ref<KRA>
   export let inline: boolean = false
@@ -14,15 +15,21 @@
   export let shrink: number = 0
 </script>
 
-<ObjectPresenter
-  objectId={value}
-  _class={performance.class.KRA}
-  {shouldShowAvatar}
-  {shouldShowName}
-  {disabled}
-  {inline}
-  {noUnderline}
-  {shrink}
-  {accent}
-  on:accent-color
-/>
+{#if value !== undefined && value !== performance.ids.NoKRARef}
+  <ObjectPresenter
+    objectId={value}
+    _class={performance.class.KRA}
+    {shouldShowAvatar}
+    {shouldShowName}
+    {disabled}
+    {inline}
+    {noUnderline}
+    {shrink}
+    {accent}
+    on:accent-color
+  />
+{:else}
+  <span>
+    <Label label={performance.string.NoKRA} />
+  </span>
+{/if}
