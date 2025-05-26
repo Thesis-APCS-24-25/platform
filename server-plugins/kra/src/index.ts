9d6b44407786147f8f2191fde66167d4b76f91ae
ltp1405 <lamtungpro1405@gmail.com>
2025-05-26 15:37:02 +0700

    add server plugins for kra

---
+++ server-plugins/kra/src/index.ts
@@ -0,0 +1,43 @@
+//
+// Copyright © 2022 Hardcore Engineering Inc.
+//
+// Licensed under the Eclipse Public License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License. You may
+// obtain a copy of the License at https://www.eclipse.org/legal/epl-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+import { Doc } from '@hcengineering/core'
+import type { Plugin, Resource } from '@hcengineering/platform'
+import { plugin } from '@hcengineering/platform'
+import { TriggerFunc } from '@hcengineering/server-core'
+import { Presenter, NotificationContentProvider } from '@hcengineering/server-notification'
+
+/**
+ * @public
+ */
+export const serverKraId = 'server-kra' as Plugin
+
+/**
+ * @public
+ */
+export default plugin(serverKraId, {
+  function: {
+    IssueHTMLPresenter: '' as Resource<Presenter>,
+    IssueTextPresenter: '' as Resource<Presenter>,
+    IssueNotificationContentProvider: '' as Resource<NotificationContentProvider>,
+    IssueLinkIdProvider: '' as Resource<(doc: Doc) => Promise<string>>
+  },
+  trigger: {
+    OnIssueUpdate: '' as Resource<TriggerFunc>,
+    OnProjectRemove: '' as Resource<TriggerFunc>,
+    OnGoalRemove: '' as Resource<TriggerFunc>,
+    OnWorkspaceOwnerAdded: '' as Resource<TriggerFunc>
+  }
+})
