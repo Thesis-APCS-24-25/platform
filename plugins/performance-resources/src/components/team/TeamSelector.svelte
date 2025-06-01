4c5c8ea2f4bd8ab356be6c90eb7b8246bdc23e53
ltp1405 <lamtungpro1405@gmail.com>
2025-06-01 23:24:31 +0700

    style performance nav header

--- plugins/performance-resources/src/components/team/TeamSelector.svelte
+++ plugins/performance-resources/src/components/team/TeamSelector.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
   import kraTeam, { Team } from '@hcengineering/kra-team'
-  import SpaceSelector from '@hcengineering/presentation/src/components/SpaceSelector.svelte'
   import performance from '../../plugin'
   import { getCurrentAccount, Ref } from '@hcengineering/core'
-  import { getClient } from '@hcengineering/presentation'
+  import { getClient, createQuery } from '@hcengineering/presentation'
   import { currentTeam as selectedTeam } from '../../utils/team'
-
-  export let currentTeam: Ref<Team> | undefined = undefined
+  import {
+    AnySvelteComponent,
+    Button,
+    eventToHTMLElement,
+    Icon,
+    IconChevronDown,
+    IconWithEmoji,
+    showPopup
+  } from '@hcengineering/ui'
+  import SpacesPopup from '@hcengineering/presentation/src/components/SpacesPopup.svelte'
+  import { ComponentType, createEventDispatcher } from 'svelte'
+  import { Asset } from '@hcengineering/platform'
+  import view from '@hcengineering/view'
 
   const me = getCurrentAccount()._id
+  const dispatch = createEventDispatcher()
   async function findDefaultSpace (): Promise<Team | undefined> {
     const client = getClient()
     $selectedTeam = localStorage.getItem(performance.string.SelectTeam) as Ref<Team> | undefined
@@ -26,20 +37,65 @@
       _id: $selectedTeam
     })
   }
+
+  let team: Team | undefined = undefined
+  const teamQ = createQuery()
+  $: teamQ.query(
+    kraTeam.class.Team,
+    {
+      _id: $selectedTeam
+    },
+    (res) => {
+      team = res[0]
+    }
+  )
+  export let iconWithEmoji: AnySvelteComponent | Asset | ComponentType | undefined = view.ids.IconWithEmoji
+  export let defaultIcon: AnySvelteComponent | Asset | ComponentType = kraTeam.icon.Team
+
+  $: void findDefaultSpace()
 </script>
 
-<SpaceSelector
-  bind:space={currentTeam}
+<Button
   size="large"
-  kind="contrast"
+  kind="primary"
   width="100%"
-  shape="round2"
-  justify="left"
-  label={performance.string.SelectTeam}
-  _class={kraTeam.class.Team}
-  query={{
-    members: me
+  justify="center"
+  icon={team?.icon === iconWithEmoji && iconWithEmoji !== undefined ? IconWithEmoji : team?.icon ?? defaultIcon}
+  iconProps={team?.icon === iconWithEmoji && iconWithEmoji !== undefined
+    ? { icon: team?.color, size: 'medium' }
+    : { size: 'medium' }}
+  on:click={(ev) => {
+    showPopup(
+      SpacesPopup,
+      {
+        _class: kraTeam.class.Team,
+        selected: $selectedTeam
+      },
+      eventToHTMLElement(ev),
+      (res) => {
+        if (res !== undefined) {
+          dispatch('change', res._id)
+        }
+      }
+    )
   }}
-  {findDefaultSpace}
-  on:change
-/>
+>
+  <svelte:fragment slot="content">
+    <span class="uppercase name">
+      {team?.name}
+    </span>
+  </svelte:fragment>
+  <svelte:fragment slot="iconRight">
+    <Icon icon={IconChevronDown} size="small" />
+  </svelte:fragment>
+</Button>
+
+<style lang="scss">
+  .name {
+    width: 100%;
+    font-weight: 600;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
+</style>
