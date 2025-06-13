3b8a303861cae814a4fc5880a26aff6f95b4eb91
ltp1405 <lamtungpro1405@gmail.com>
2025-06-13 15:35:52 +0700

    fix UI

--- plugins/performance-resources/src/components/application/Navigator.svelte
+++ plugins/performance-resources/src/components/application/Navigator.svelte
@@ -62,7 +62,7 @@
 <Scroller shrink>
   {#if activeReviewSessionModel === undefined || draftingReviewSessionModel === undefined || concludedReviewSessionModel === undefined}
     <Label label={performance.string.NoTeam} />
-  {:else if currentSpace === undefined}
+  {:else}
     <ActiveReviewSessionNav
       {reviewSessions}
       model={activeReviewSessionModel}
